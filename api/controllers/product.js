2ad3ae2b02ed61c551c2e6ff1ad2d92c710616f3
Abhinav Kumar Jha <akj0712@duck.com>
2022-11-30 16:51:35 +0530

    added nodemon

--- api/controllers/product.js
+++ api/controllers/product.js
@@ -1,4 +1,4 @@
-import Product from "../models/product.js";
+import Product from "../models/Product.js";
 import { ApiFeatures } from "../utils/apiFeatures.js";
 import { createError } from "../utils/error.js";
 
